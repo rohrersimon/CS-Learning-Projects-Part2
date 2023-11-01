cdc3b7ecc04f664a4079e3ada1c5365f59614801
Simon Rohrer <simon.rohrer@live.com>
2023-11-01 18:24:03 +0000

    Course 4

--- Course 4.cs
+++ Course 4.cs
@@ -17,15 +17,18 @@ public class Course4
 
         // Your logic here
         Console.WriteLine($"Dear {customerName},");
-        Console.WriteLine($"");
-        Console.WriteLine($"");
-        Console.WriteLine($"");
+        Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+
+        Console.WriteLine($"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.\n");
+        Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C2}.\n");
 
         Console.WriteLine("Here's a quick comparison:\n");
 
         string comparisonMessage = "";
 
         // Your logic here
+        comparisonMessage += $"{currentProduct}\t\t{currentReturn:P2}\t{currentProfit:C2}\n";
+        comparisonMessage += $"{newProduct}\t\t{newReturn:P2}\t{newProfit:C2}\n";
 
         Console.WriteLine(comparisonMessage);
     }
